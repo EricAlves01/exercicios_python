ccedae1438c94686c3422df6e1feda582e5056ed
EricAlves01 <ericviniciusw0712@gmail.com>
2026-05-20 15:55:57 -0400

    3 novos exercicios resolvidos

---
+++ 01_conceitos_basicos/exe08.py
@@ -0,0 +1,8 @@
+# aula 08 - conversor de medidas de metros para centimentros e milimetros : 
+
+metros = float(input('digite uma distancia em metros: '))
+
+c = metros * 100 
+m = metros * 1000 
+
+print(f" de metros {metros} m\nConvertido para Centimentros: {c} cm || Milimetros: {m} mm")
